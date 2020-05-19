b48608882d67d65f1e5e09da1b5dca76c9c0606b
Aditya Singh <samant04aditya@gmail.com>
2020-05-19 18:52:16 +0530

    flare animations: better ✔

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'contents.dart';
 import 'result_page.dart';
-import 'calculator.dart';
+import 'result_calc.dart';
 
 
 class InputPage extends StatefulWidget {
